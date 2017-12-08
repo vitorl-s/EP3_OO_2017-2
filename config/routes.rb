f41a6d8b22883c497b9b8f72169a3d4a311c4da1
Vitor Leal <vitor_leal2201@hotmail.com>
2017-12-08 19:12:23 -0200

    Adição de CSS em algumas partes do site

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :users
-  resources :cardapios 
+  resources :cardapios
   get 'home' => 'home#home'
   get 'cardapio1' => 'home#cardapio1'
   get 'entrar' => 'home#entrar'
   get 'cadastro' => 'home#cadastro'
   get "fale_conosco" => 'home#fale_conosco'
   root :to => 'home#home'
-
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207161758) do
+ActiveRecord::Schema.define(version: 20171208180027) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cardapios", force: :cascade do |t|
     t.string "nome"
@@ -34,6 +49,7 @@ ActiveRecord::Schema.define(version: 20171207161758) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
