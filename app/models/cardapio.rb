b80f84be4d9a43aad9aba4a60333ef0ee469f213
Vitor Leal <vitor_leal2201@hotmail.com>
2017-12-09 03:27:29 -0200

    Adição do shopping cart

--- app/models/cardapio.rb
+++ app/models/cardapio.rb
@@ -2,4 +2,9 @@ class Cardapio < ApplicationRecord
   validates :nome , :descricao , :preco , :link_imagem , presence: true
   validates :preco , numericality: true
 
+  def attr(contador)
+    valor = contador.to_f
+    return valor
+  end
+
 end
