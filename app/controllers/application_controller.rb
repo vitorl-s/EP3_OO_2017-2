2ab5fd1a722bb6f0c1a94978c35546f2c8c111c1
Vitor Leal <vitor_leal2201@hotmail.com>
2017-12-10 17:15:50 -0200

    Correção da mensagem de alerta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def checar_adm()
+    if user_signed_in?
+    if current_user.email == 'vitor@gmail.com' || current_user.email == 'lclaudio.tl@gmail.com'
+    end
+  end
+end
 end
